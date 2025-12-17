80d4f9d3e0c0dc3b6473edf621165aa204b92508
Avetis Berberyan <avetis.berberyan.techx@gmail.com>
2025-12-17 10:42:56 +0400

    add logs to see data types

--- personalInfoDashboard.js
+++ personalInfoDashboard.js
@@ -12,6 +12,7 @@ const hobbies = [
 ];
 
 const contact = {
+
   email: 'avetisberberyan72@gmail.com',
   phone: '077793902',
   city: 'Stepanavan',
@@ -20,51 +21,82 @@ const contact = {
 // Introduction
 const age = currentYear - birthYear;
 
-console.log(`Hi, my name is ${firstName} ${lastName}. I live in ${contact.city}.`);
-console.log(`I am ${age} years old and currently ${isStudent ? 'a student' : 'not a student'}.`);
+console.log(`Hi, my name is ${
 
-// Comparison: == vs ===
+firstName
+} ${
+
+lastName
+}. I live in ${
+
+contact.city
+}.`);
+console.log(`I am ${
+
+age
+} years old and currently ${
+
+isStudent ? 'a student' : 'not a student'
+}.`);
+
 const ageString = '25';
 const ageNumber = 25;
 
+//types
+
+console.log(`type of ageString ${
+
+typeof ageString
+}`);
+console.log(`type of ageNumber ${
+
+typeof ageNumber
+}`);
+
+// Comparison: == vs ===
 console.log(ageString == ageNumber);   // true (type coercion)
 console.log(ageString === ageNumber);  // false (strict)
 
 // Grading
 const score = 55;
-let grade;
 
 if (score < 60) {
+
   grade = 'F';
 } else if (score >= 60 && score <= 69) {
+
   grade = 'D';
 } else if (score >= 70 && score <= 79) {
+
   grade = 'C';
 } else if (score >= 80 && score <= 89) {
+
   grade = 'B';
 } else if (score >= 90 && score <= 100) {
+
   grade = 'A';
 }
 
 // Grade comment
 switch (grade) {
-  case 'F':
-    console.log('Needs serious effort');
-    break;
-  case 'D':
-    console.log('Try harder');
-    break;
-  case 'C':
-    console.log('Keep improving');
-    break;
-  case 'B':
-    console.log('Good job');
-    break;
-  case 'A':
-    console.log('Excellent work');
-    break;
   default:
     console.log('Invalid score');
+
+case 'F':
+    console.log('Needs serious effort');
+break;
+case 'D':
+    console.log('Try harder');
+break;
+case 'C':
+    console.log('Keep improving');
+break;
+case 'B':
+    console.log('Good job');
+break;
+case 'A':
+    console.log('Excellent work');
+break;
 }
 
 // Final result
